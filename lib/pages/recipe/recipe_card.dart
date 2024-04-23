ee783ba4db9647aa4417f6315a599896d3b8a782
yi.fang.23@ucl.ac.uk <ff@FYdeMacBook-Air.local>
2024-04-23 03:54:50 +0100

    Update version

--- lib/pages/recipe/recipe_card.dart
+++ lib/pages/recipe/recipe_card.dart
@@ -94,7 +94,7 @@ class RecipeCard extends StatelessWidget {
             borderRadius: const BorderRadius.vertical(
                 top: Radius.circular(12)), // 图片顶部圆角。
             child: AspectRatio(
-              aspectRatio: 2,
+              aspectRatio: 1.5,
               child: SizedBox(
                 width: double.infinity,
                 child: Image.network(
